927b41ad9e49da02a0c3849b41669fe704c18892
Moad <moad.makboul.mm@gmail.com>
2020-06-25 14:13:34 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./searchParams";
+import Details from "./Details";
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
